77188ee5d357fefc034cd9a3cf7c3cc2a8274cb7
Isaac Flores <isaac.a.flores2@gmail.com>
2019-08-27 16:48:14 -0700

    Completed quickSort implementation

---
+++ SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class QuickSort : SortIntList
+    {
+        public QuickSort(List<int> items) : base(items)
+        {
+        }
+
+        public override IList<int> Sort()
+        {
+            int initialPivotIndex = selectPivotIndex();
+            quickSort(0, initialPivotIndex, initialPivotIndex);
+            return Items;
+        }
+
+        protected int selectPivotIndex()
+        {
+            return Items.Count() - 1;            
+        }
+
+        protected void quickSort(int leftIndex, int rightIndex, int pivotIndex)
+        {            
+            int newPivot = partition(leftIndex, rightIndex, pivotIndex);
+
+            if (newPivot == -1)
+                return;
+
+            quickSort(leftIndex, newPivot - 1, newPivot -1);
+            quickSort(newPivot + 1, rightIndex, rightIndex);
+        }
+
+        protected int partition(int leftIndex, int rightIndex, int pivotIndex)
+        {
+            if (pivotIndex < 0 || (leftIndex == rightIndex ) || (leftIndex > rightIndex) )
+            {
+                return -1;
+            }
+
+            int pivotPoint = leftIndex;
+            int pivotValue = Items[pivotIndex];
+
+            for(int i = leftIndex; i < rightIndex; i++)
+            {
+                if(Items[i] < pivotValue)
+                {                    
+                    swap(i, pivotPoint);
+                    pivotPoint++;
+                }               
+            }
+
+            if (pivotValue < Items[pivotPoint])
+            {
+                swap(pivotPoint, pivotIndex);
+            }
+
+            return pivotPoint;
+        }
+    }
+}
---
+++ SortingAlgorithmsTests/QuickSortTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms.Tests
+{
+    [TestClass()]
+    public class QuickSortTests
+    {        
+        [TestMethod()]
+        public void SortTest()
+        {
+            List<int> items = new List<int> { 40, 20, 30, 2, 5, 90, 15 };
+            List<int> expectedItems = new List<int> { 2, 5, 15, 20, 30, 40, 90 };
+
+            QuickSort quickSort = new QuickSort(items);
+            items = (List<int>) quickSort.Sort(); 
+
+            CollectionAssert.AreEqual(expectedItems, items);            
+        }
+    }
+}
