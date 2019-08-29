506701118781d861eec509ef26b6dc0258ddf33a
Isaac Flores <34590010+isaacaflores2@users.noreply.github.com>
2019-08-29 16:24:48 -0700

    Completed bucketSort implementation

--- SortingAlgorithms/BucketSort.cs
+++ SortingAlgorithms/BucketSort.cs
@@ -6,55 +6,93 @@ using System.Threading.Tasks;
 
 namespace SortingAlgorithms
 {
-    public class BucketSort : SortIntList
+    public static class BucketSort
     {
-        private List<LinkedList<int>> buckets;
-
-        public BucketSort(List<int> items): base(items)
+        static List<LinkedList<int>> buckets;
+        static List<int> Items {get; set;}
+        static int NumBuckets { get; set; }
+      
+        public static IList<int> Sort(List<int> items, int numBuckets)
         {
-        }
-
-        public override IList<int> Sort()
-        {
-            createBuckets(); 
-            
-            foreach(LinkedList<int> bucket in buckets)
-            {
-                sortBucket(bucket);
-            }
-
-            return Items; 
-        }
-
-        protected IList<int> createBuckets()
-        {
-            buckets = new List<LinkedList<int>>();
-            int itemsSize = Items.Count();
-
-            foreach( int item in Items)
-            {
-                int bucketIndex = item % itemsSize;
-                addItemToBucket(item, bucketIndex);
-            }
-
-            mergeBuckets();
+            Items = items;
+            NumBuckets = numBuckets;
+            createBuckets();
+            sortAllBuckets();
+            mergeAllBuckets();
             return Items;
         }
 
-        private void addItemToBucket(int item, int bucketIndex)
+        static void createBuckets()
+        {
+            buckets = new List<LinkedList<int>>();
+            initializeBuckets();
+            addAllItemsToBuckets();
+        }
+        
+        static void initializeBuckets()
+        {
+            for(int i = 0; i < NumBuckets; i++)
+            {
+                buckets.Add(null);
+            }
+        }
+
+        static void addAllItemsToBuckets()
+        {
+            foreach (int item in Items)
+            {
+                int bucketIndex = item % NumBuckets;
+                addItemToBucket(item, bucketIndex);
+            }
+        }
+
+        static void addItemToBucket(int item, int bucketIndex)
         {
             LinkedList<int> bucketLinkedList = buckets[bucketIndex];
+            if(bucketLinkedList == null)
+            {
+                bucketLinkedList = new LinkedList<int>(); 
+            }            
+            bucketLinkedList.AddLast(item);
+            buckets[bucketIndex] = bucketLinkedList;
         }
 
-        protected void sortBucket(LinkedList<int> linkedList)
+        static void sortAllBuckets()
         {
-            throw new NotImplementedException();           
+            foreach (LinkedList<int> bucket in buckets)
+            {
+                if(bucket != null )
+                    sortBucket(bucket);
+            }
         }
 
-        protected void mergeBuckets()
+        static void sortBucket(LinkedList<int> linkedList)
         {
-            throw new NotImplementedException();
+            List<int> bucketList = new List<int>(linkedList);
+            QuickSort quickSort = new QuickSort(bucketList);
+            bucketList = (List<int>) quickSort.Sort();
+            linkedList = new LinkedList<int>(bucketList);
         }
 
+        static void mergeAllBuckets()
+        {
+            int itemsIndex = 0; 
+            foreach (LinkedList<int> bucket in buckets)
+            {
+                if(bucket != null)
+                    itemsIndex = mergeBucket(bucket, itemsIndex);                 
+            }
+        }
+
+        static int mergeBucket(LinkedList<int> bucket, int startIndex)
+        {
+            int itemsIndex = startIndex; 
+            foreach(int item in bucket)
+            {
+                Items[itemsIndex] = item; 
+                itemsIndex++; 
+            }
+            return itemsIndex; 
+        }
     }
 }
