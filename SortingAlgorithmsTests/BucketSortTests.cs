bd126f38f2dc0f2b9c3b5cfda791958f9f7395a6
Isaac Flores <34590010+isaacaflores2@users.noreply.github.com>
2019-08-29 16:24:48 -0700

    Completed bubbleSort

--- SortingAlgorithmsTests/BucketSortTests.cs
+++ SortingAlgorithmsTests/BucketSortTests.cs
@@ -14,12 +14,11 @@ namespace SortingAlgorithms.Tests
         [TestMethod()]
         public void SortTest()
         {
-            List<int> items = new List<int> { 40, 20, 30, 2, 5, 90, 15 };
-            List<int> expectedItems = new List<int> { 2, 5, 15, 20, 30, 40, 90 };
+            List<int> items = new List<int> { 1, 5, 3, 9, 5, 6, 8, 7, 2, 8 };
+            List<int> expectedItems = new List<int> {1, 2, 3, 5, 5, 6, 7, 8, 8, 9};
 
-            
-            BucketSort bucketSort = new BucketSort(items);
-            items = (List<int>) bucketSort.Sort();
+            int numBuckets = 10;
+            items = (List<int>) BucketSort.Sort(items, numBuckets);
 
             CollectionAssert.AreEqual(expectedItems, items);
         }
